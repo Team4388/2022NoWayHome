f9a12a3b7af661323704a061c7a63985530569a4
66945 <66945@psdschools.org>
2022-01-28 16:27:11 -0700

    Added limit switches
    and auto open

--- src/main/java/frc4388/robot/subsystems/Hooks.java
+++ src/main/java/frc4388/robot/subsystems/Hooks.java
@@ -1,5 +1,6 @@
 package frc4388.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,16 +10,23 @@ public class Hooks extends SubsystemBase {
   private CANSparkMax m_leftHook;
   private CANSparkMax m_rightHook;
 
-  double m_leftOffset;
-  double m_rightOffset;
+  private LimitSwitchNormal m_limitSwitch;
+
+  private double m_leftOffset;
+  private double m_rightOffset;
 
   private boolean m_open;
 
-  public Hooks(CANSparkMax leftHook, CANSparkMax rightHook) {
+  public Hooks(CANSparkMax leftHook, CANSparkMax rightHook, LimitSwitchNormal limitSwitch) {
     m_leftHook = leftHook;
     m_rightHook = rightHook;
 
-    setOpen(false);
+    m_limitSwitch = limitSwitch;
+
+    m_open = false;
+
+    m_leftHook.set(.1);
+    m_rightHook.set(.1);
   }
 
   public void setOpen(boolean open) {
@@ -35,5 +43,13 @@ public class Hooks extends SubsystemBase {
 
   public boolean getOpen() {
     return m_open;
+  }
+
+  @Override
+  public void periodic() {
+    if(m_limitSwitch.compareTo(LimitSwitchNormal.NormallyClosed) == 1) {
+      m_leftOffset = m_leftHook.getEncoder().getPosition();
+      m_leftOffset = m_leftHook.getEncoder().getPosition();
+    }
   }
 }
