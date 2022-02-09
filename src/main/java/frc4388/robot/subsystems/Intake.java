44870c8aa13ca5c9fc89f680caf7c83af9d4158b
aarav18 <aaravshah18@gmail.com>
2022-02-08 17:24:17 -0700

    limit switches

--- src/main/java/frc4388/robot/subsystems/Intake.java
+++ src/main/java/frc4388/robot/subsystems/Intake.java
@@ -14,15 +14,14 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxLimitSwitch;
 import com.revrobotics.CANSparkMax.IdleMode;
 
 public class Intake extends SubsystemBase {
 
   private WPI_TalonFX m_intakeMotor;
   private CANSparkMax m_extenderMotor;
-  // private SparkMaxLimitSwitch m_inLimit;
-  // private SparkMaxLimitSwitch m_outLimit;
+  private SparkMaxLimitSwitch m_inLimit;
+  private SparkMaxLimitSwitch m_outLimit;
 
   public boolean toggle;
 
@@ -31,17 +30,16 @@ public class Intake extends SubsystemBase {
     m_intakeMotor = intakeMotor;
     m_extenderMotor = extenderMotor; 
 
-    
     m_extenderMotor.restoreFactoryDefaults();
 
     m_intakeMotor.setNeutralMode(NeutralMode.Brake);
     m_intakeMotor.setInverted(false);
     m_extenderMotor.setInverted(true);
     
-    // m_inLimit = m_extenderMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
-    // m_outLimit = m_extenderMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
-    // m_inLimit.enableLimitSwitch(true);
-    // m_outLimit.enableLimitSwitch(true);
+    m_inLimit = m_extenderMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+    m_outLimit = m_extenderMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+    m_inLimit.enableLimitSwitch(true);
+    m_outLimit.enableLimitSwitch(true);
   }
 
   @Override
