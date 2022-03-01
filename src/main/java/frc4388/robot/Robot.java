e2f637675674a84a0d447bc32527caeb3398a339
Aarav Shah <aaravshah18@gmail.com>
2022-02-28 18:55:42 -0700

    Update Robot.java

--- src/main/java/frc4388/robot/Robot.java
+++ src/main/java/frc4388/robot/Robot.java
@@ -30,7 +30,6 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    //AimToCenterTest.RunAll();
     m_robotContainer = new RobotContainer();
   }
 
