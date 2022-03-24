d11949bcae3cf0147393e65fb6cd067bba2479dd
nathanrsxtn <nathanrsxtn@gmail.com>
2022-03-24 06:53:40 -0600

    added limit switch and soft limit
    -ryan

--- src/main/java/frc4388/robot/subsystems/Climber.java
+++ src/main/java/frc4388/robot/subsystems/Climber.java
@@ -8,16 +8,35 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc4388.robot.Constants.ClimberConstants;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+
 public class Climber extends SubsystemBase {
   private WPI_TalonFX elbow;
 
   /** Creates a new Climber */
-  public Climber(WPI_TalonFX elbow) { this.elbow = elbow; }
-  
-  public void setEncoders(double value) { this.elbow.setSelectedSensorPosition(value); }
-  public double getCurrent() { return this.elbow.getSupplyCurrent(); }
-  public void setMotors(double elbowOutput) { this.elbow.set(elbowOutput * ClimberConstants.INPUT_MULTIPLIER); }
+  public Climber(WPI_TalonFX elbow) {
+    this.elbow = elbow;
+    elbow.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
+    elbow.overrideLimitSwitchesEnable(true);
+    
+    elbow.configForwardSoftLimitThreshold(ClimberConstants.ELBOW_FORWARD_SOFT_LIMIT);
+    elbow.configForwardSoftLimitEnable(true);
+  }
+
+  public void setEncoders(double value) {
+    this.elbow.setSelectedSensorPosition(value);
+  }
+
+  public double getCurrent() {
+    return this.elbow.getSupplyCurrent();
+  }
+
+  public void setMotors(double elbowOutput) {
+    this.elbow.set(elbowOutput * ClimberConstants.INPUT_MULTIPLIER);
+  }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+  }
 }
